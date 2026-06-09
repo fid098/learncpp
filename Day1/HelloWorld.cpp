13bf6691d30ce569df2b5d04e71f3c24e33140ed
Fidel <fidel.ehirim@student.manchester.ac.uk>
2026-06-09 17:21:42 +0100

    Definition and Assignment

--- Day1/HelloWorld.cpp
+++ Day1/HelloWorld.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
     std::cout << "Hello, World!";
+    std::cin.clear(); // reset any error flags
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    // ignore any characters in the input buffer until we find a newline
+    std::cin.get();
+    // waits for the user to press a key,(gets one more char from the user)
     return 0;
 }
-
